6274c69b156be39d01859767ffbca9d38b75709a
nietoperz0 <vipciorr@gmail.com>
2023-10-06 01:55:40 +0200

    change to new logic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,32 +13,45 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent {
   //DEFAULT
-  DEFAULT_TIMER: number = 25;
-  DEFAULT_BREAK: number = 5;
-  DEFAULT_LONG_BREAK: number = 20;
+  DEFAULT_TIMER: number = 20 * 60;
+  DEFAULT_BREAK: number = 5 * 60;
+  DEFAULT_LONG_BREAK: number = 20 * 60;
   // -----
 
   counter: number = 1;
   // blocks: BlocksType[] = [];
-  minutes: any = 25;
-  seconds: any = '00';
+  // minutes: any = 25;
+  // seconds: any = '00';
 
   timerStarted: boolean = false;
   timeToBreak: boolean = false;
 
   interval: any;
 
+  //NEW
+  timer: number = this.DEFAULT_TIMER;
+
+  show_min: string = '';
+  show_sec: string = '';
+
   constructor(private titleService: Title) {
     // titleService.setTitle('Pomidoro');
+    this.updateTimer();
   }
 
   increaseTimer(): void {
-    this.minutes++;
+    // this.minutes++;
+    this.timer += 60;
+    this.updateTimer();
   }
 
   decreaseTimer(): void {
-    if (this.minutes > 0) {
-      this.minutes--;
+    // if (this.minutes > 0) {
+    //   this.minutes--;
+    // }
+    if (this.timer > 0) {
+      this.timer -= 60;
+      this.updateTimer();
     }
   }
 
@@ -49,47 +62,85 @@ export class HomeComponent {
 
   resetTimer(): void {
     this.stopTimer();
-    this.minutes = this.DEFAULT_TIMER;
-    this.seconds = '00';
+    // this.minutes = this.DEFAULT_TIMER;
+    // this.seconds = '00';
+    this.timer = this.DEFAULT_TIMER;
     this.counter = 1;
     this.timeToBreak = false;
+    this.updateTimer();
     // this.blocks = [];
+  }
+
+  updateTimer(): void {
+    // MINUTES
+    let minutes = this.timer / 60;
+    if (minutes >= 1) {
+      this.show_min = Math.floor(minutes).toString();
+    } else {
+      this.show_min = '0' + Math.floor(minutes).toString();
+    }
+
+    // SECONDS
+    let seconds = this.timer % 60;
+    if (seconds >= 10) {
+      this.show_sec = seconds.toString();
+    } else {
+      this.show_sec = '0' + seconds.toString();
+    }
+
+    // UPDATE TITLE
+    this.updateTitle(this.show_min, this.show_sec);
   }
 
   startTimer(): void {
     this.timerStarted = true;
 
-    this.interval = setInterval(() => counter(), 1000);
+    // this.interval = setInterval(() => counter(), 1000);
+    this.interval = setInterval(() => counter2(), 1000);
 
-    const counter = (): void => {
-      if (this.seconds == 0) {
-        this.minutes--;
-        if (this.minutes <= 9) {
-          this.minutes = '0' + this.minutes;
-        }
-        this.seconds = 60;
-      }
-      this.seconds--;
-      if (this.seconds <= 9 && this.seconds > 0) {
-        this.seconds = '0' + this.seconds;
-        console.log(this.seconds);
-      }
+    const counter2 = (): void => {
+      this.timer--;
+      this.updateTimer();
 
-      if (this.minutes == 0 && this.seconds == 0) {
+      console.log(this.show_min);
+      console.log(this.show_sec);
+
+      if (this.timer <= 0) {
         this.stopTimer();
-        // this.resetTimer();
         this.playRinging();
         this.nextTimer();
-        // this.manageBlocks(false);
       }
-
-      this.updateTitle();
     };
+
+    // const counter = (): void => {
+    //   if (this.seconds == 0) {
+    //     this.minutes--;
+    //     if (this.minutes <= 9) {
+    //       this.minutes = '0' + this.minutes;
+    //     }
+    //     this.seconds = 60;
+    //   }
+    //   this.seconds--;
+    //   if (this.seconds <= 9 && this.seconds > 0) {
+    //     this.seconds = '0' + this.seconds;
+    //     console.log(this.seconds);
+    //   }
+
+    //   if (this.minutes == 0 && this.seconds == 0) {
+    //     this.stopTimer();
+    //     // this.resetTimer();
+    //     this.playRinging();
+    //     this.nextTimer();
+    //     // this.manageBlocks(false);
+    //   }
+
+    //   this.updateTitle();
+    // };
   }
 
-  startBreak(): void {
-    this.timerStarted = true;
-  }
+  // startBreak(): void {
+  //   this.timerStarted = true;
+  // }
 
   // manageBlocks(skip: boolean): void {
   //   // this.block++;
@@ -113,27 +164,27 @@ export class HomeComponent {
     this.timeToBreak = !this.timeToBreak;
 
     if (this.timeToBreak === true) {
-      this.minutes = this.DEFAULT_BREAK;
+      this.timer = this.DEFAULT_BREAK;
     }
 
     if (this.timeToBreak === true && this.counter % 4 === 0) {
       // console.log('long przerwa');
       // this.block++;
-      this.minutes = this.DEFAULT_LONG_BREAK;
+      this.timer = this.DEFAULT_LONG_BREAK;
     }
 
     if (this.timeToBreak === false) {
       this.counter++;
-      this.minutes = this.DEFAULT_TIMER;
+      this.timer = this.DEFAULT_TIMER;
     }
-    this.seconds = '00';
-    this.updateTitle();
+
+    this.updateTimer();
+    // this.seconds = '00';
+    // this.updateTitle();
   }
 
-  updateTitle(): void {
-    this.titleService.setTitle(
-      `${this.minutes}:${this.seconds} - Pomodoro Timer 🍅`
-    );
+  updateTitle(minutes: string, seconds: string): void {
+    this.titleService.setTitle(`${minutes}:${seconds} - Pomodoro Timer 🍅`);
   }
 
   playRinging(): void {
